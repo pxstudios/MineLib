3c3ae8523c91531b423c7e797338cdb99997df19
itzstonlex <seltixfromhell@bk.ru>
2022-07-19 19:40:38 +0400

    fill EventListenerRegistryObjectAdapter object-instance calling

--- src/main/java/net/pxstudios/minelib/registry/adapter/type/EventListenerRegistryObjectAdapter.java
+++ src/main/java/net/pxstudios/minelib/registry/adapter/type/EventListenerRegistryObjectAdapter.java
@@ -1,20 +1,66 @@
 package net.pxstudios.minelib.registry.adapter.type;
 
-import net.pxstudios.minelib.command.type.AbstractContextCommand;
+import net.pxstudios.minelib.MineLibrary;
+import net.pxstudios.minelib.event.SingleEventBuilder;
 import net.pxstudios.minelib.registry.BukkitRegistryObject;
 import net.pxstudios.minelib.registry.adapter.BukkitRegistryObjectAdapter;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 
+import java.lang.reflect.Method;
+import java.util.function.Consumer;
+
 public class EventListenerRegistryObjectAdapter extends BukkitRegistryObjectAdapter<Listener> {
+    private final MineLibrary mineLibrary = MineLibrary.getLibrary();
 
     @Override
     public void fireRegister(Plugin plugin, Listener obj) {
-        plugin.getServer().getPluginManager().registerEvents(obj, plugin);
+        if (obj != null) {
+            plugin.getServer().getPluginManager().registerEvents(obj, plugin);
+        }
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    public Listener newObjectInstance(BukkitRegistryObject<Listener> bukkitRegistryObject) {
+    public Listener newObjectInstance(Plugin plugin, BukkitRegistryObject<Listener> bukkitRegistryObject) {
+        for (Method method : bukkitRegistryObject.getClass().getMethods()) {
+            if (method.getParameterCount() != 1) {
+                continue;
+            }
+
+            if (method.isAnnotationPresent(EventHandler.class)) {
+
+                EventHandler eventHandler = method.getAnnotation(EventHandler.class);
+                Class<?> parameter = method.getParameterTypes()[0];
+
+                if (!parameter.isAssignableFrom(Event.class)) {
+                    continue;
+                }
+
+                subscribe((Class<? extends Event>) parameter, eventHandler, event -> {
+
+                    try {
+                        method.invoke(bukkitRegistryObject, event);
+                    }
+                    catch (Exception exception) {
+                        exception.printStackTrace();
+                    }
+                });
+            }
+        }
         return null;
     }
+
+    private <E extends Event> void subscribe(Class<E> cls, EventHandler eventHandler, Consumer<E> completable) {
+        SingleEventBuilder<E> singleEventBuilder = mineLibrary.getEventsSubscriber().subscribe(cls, eventHandler.priority());
+
+        if (eventHandler.ignoreCancelled()) {
+            singleEventBuilder.ignoreCancelled();
+        }
+
+        singleEventBuilder.complete(completable);
+    }
+
 }
