200182e9f07fff76bf4dffe024a814cbdcf476eb
itzstonlex <seltixfromhell@bk.ru>
2022-07-19 20:14:26 +0400

    add @SettingsMethod, @Alias in CommandRegistryObject

--- src/main/java/net/pxstudios/minelib/registry/type/CommandRegistryObject.java
+++ src/main/java/net/pxstudios/minelib/registry/type/CommandRegistryObject.java
@@ -3,5 +3,28 @@ package net.pxstudios.minelib.registry.type;
 import net.pxstudios.minelib.command.type.AbstractContextCommand;
 import net.pxstudios.minelib.registry.BukkitRegistryAdaptiveObject;
 
+import java.lang.annotation.*;
+
 public class CommandRegistryObject extends BukkitRegistryAdaptiveObject<AbstractContextCommand> {
+
+    @Target({ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface SettingsMethod {
+    }
+
+    @Repeatable(MultipleAliases.class)
+    @Target({ElementType.TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Alias {
+
+        String value();
+    }
+
+    @Target({ElementType.TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface MultipleAliases {
+
+        Alias[] value();
+    }
+
 }
