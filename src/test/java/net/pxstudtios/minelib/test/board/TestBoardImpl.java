3904ce0bf68a938b23d9089e1a5a5d1ce26b82f8
itzstonlex <seltixfromhell@bk.ru>
2022-07-24 08:37:59 +0400

    add TestBoardImpl

---
+++ src/test/java/net/pxstudtios/minelib/test/board/TestBoardImpl.java
@@ -0,0 +1,60 @@
+package net.pxstudtios.minelib.test.board;
+
+import lombok.NonNull;
+import net.pxstudios.minelib.MineLibrary;
+import net.pxstudios.minelib.common.board.Board;
+import net.pxstudios.minelib.common.board.BoardApi;
+import net.pxstudios.minelib.common.board.BoardFlag;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.DisplaySlot;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TestBoardImpl extends Board {
+
+    private static final TestBoardImpl IMPL = new TestBoardImpl(MineLibrary.getLibrary());
+
+    public static void setPlayer(@NonNull Player player) {
+        // Show board to the player.
+        IMPL.addPlayerView(player);
+    }
+
+    private TestBoardImpl(@NonNull MineLibrary mineLibrary) {
+        super(mineLibrary.getBoardApi(), DisplaySlot.SIDEBAR, "hub", BoardApi.STANDARD_CRITERIA);
+
+        addFlag(BoardFlag.REMOVE_ON_PLAYER_JOIN);
+        addFlag(BoardFlag.REMOVE_ON_CHANGED);
+        addFlag(BoardFlag.WITH_AUTOMATICALLY_STATIC_LINES_COLORIZE);
+        addFlag(BoardFlag.USE_DISABLED_WORLD_SYSTEM);
+
+        subscribePlayerEvents(mineLibrary.getEventsSubscriber());
+
+        // Add disabled worlds.
+        addDisabledWorld("survival");
+        addDisabledWorld("bw_arena_1");
+
+        // Set a display-name for objective of board.
+        setDisplayName("HUB"); // with presets support
+
+        // Set a lines for objective of board.
+        setLineSmart(5);
+        setLineSmart(4, "&7Your name:");
+        setLineSmart(3, HumanEntity::getName);
+        setLineSmart(2);
+        setLineSmart(1, "preset:Website");
+
+        // Add automatically updater to board.
+        update(10, () -> {
+
+            ChatColor[] chatColors = ChatColor.values();
+            ChatColor randomColor = chatColors[ThreadLocalRandom.current().nextInt(chatColors.length)];
+
+            setDisplayName(randomColor + ChatColor.stripColor(getObjective().getDisplayName()));
+
+            return true; // if continue the next repeat of update-action?
+        });
+    }
+
+}
