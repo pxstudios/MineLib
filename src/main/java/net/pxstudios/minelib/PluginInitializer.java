c54e41646f2d28a9774caa44a4fea050cc009bca
itzstonlex <seltixfromhell@bk.ru>
2022-07-25 18:09:51 +0400

    PluginInitializer: add logging

--- src/main/java/net/pxstudios/minelib/PluginInitializer.java
+++ src/main/java/net/pxstudios/minelib/PluginInitializer.java
@@ -1,11 +1,18 @@
 package net.pxstudios.minelib;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PluginInitializer extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getLogger().info(ChatColor.AQUA + "PluginInitializer has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info(ChatColor.RED + "PluginInitializer has been disabled!");
     }
 
 }
