c3c82dc50251d0898bcc3cb84d65ef86a0c5a9e7
itzstonlex <seltixfromhell@bk.ru>
2022-07-26 16:32:50 +0400

    upgrade GuiSlot functional

--- src/main/java/net/pxstudios/minelib/gui/GuiSlot.java
+++ src/main/java/net/pxstudios/minelib/gui/GuiSlot.java
@@ -2,12 +2,16 @@ package net.pxstudios.minelib.gui;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import net.pxstudios.minelib.common.location.point.Point2D;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class GuiSlot {
 
     public static GuiSlot byArray(int slotIndex) {
-        return new GuiSlot(slotIndex);
+        int y = slotIndex / 9;
+        int x = slotIndex - (y * 9);
+
+        return new GuiSlot(slotIndex, new Point2D(x, y));
     }
 
     public static GuiSlot bySlot(int slot) {
@@ -15,7 +19,7 @@ public class GuiSlot {
     }
 
     public static GuiSlot byMatrixArray(int x, int y) {
-        return new GuiSlot((y + 1) * 9 - (9 - x));
+        return new GuiSlot((y + 1) * 9 - (9 - x), new Point2D(x, y));
     }
 
     public static GuiSlot byMatrix(int x, int y) {
@@ -23,8 +27,26 @@ public class GuiSlot {
     }
 
     private final int slot;
+    private final Point2D point2D;
 
-    public final int normalize() {
+    public final int toArraySlot() {
+        return slot;
+    }
+
+    public final int toSlot() {
         return slot + 1;
     }
+
+    public final int toArrayX() {
+        return (int) point2D.x();
+    }
+
+    public final int toArrayY() {
+        return (int) point2D.y();
+    }
+
+    public Point2D toPoint2D() {
+        return point2D;
+    }
+
 }
